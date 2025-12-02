1dd660923f2eb36b67e78c38eda582d61c796fa7
Artem Horbeshko <artemhorbeshko777@gmail.com>
2025-12-02 19:59:45 +0100

    hw15

---
+++ docs/homework15/js/splide.js
@@ -0,0 +1,31 @@
+var splide = new Splide(".splide", {
+  arrows: false,
+  pagination: false,
+});
+splide.mount();
+// Все кнопки
+var buttons = document.querySelectorAll("#custom-pagination [data-slide]");
+
+// При клике — переходим на выбранный слайд
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    var index = parseInt(button.getAttribute("data-slide"));
+    splide.go(index);
+  });
+});
+
+splide.on("move", function (newIndex) {
+  buttons.forEach((btn) => btn.classList.remove("active"));
+  buttons[newIndex].classList.add("active");
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Splide('.testimonials', {
+    type: 'loop',
+    perPage: 1,
+    pagination: true,
+    arrows: false,
+    autoplay: false,
+    interval: 5000,
+  }).mount();
+});
