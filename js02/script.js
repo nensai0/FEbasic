78079b4647264b3bbaf5ca34213a9da677159724
Artem Horbeshko <artemhorbeshko777@gmail.com>
2025-12-13 21:58:39 +0100

    js02

---
+++ js02/script.js
@@ -0,0 +1,34 @@
+var obj = {
+    name: 'ivan',
+    age: 28,
+    city: {
+        name: 'KH',
+        code: '654654',
+    }
+}
+console.log(obj)
+
+var message = 'Hello';
+var errorMessage = 'Hello';
+
+var title;
+var number;
+var page;
+var category;
+
+var windowSize;
+var strLength;
+var orderNumber;
+var itemsCount;
+var currentStep;
+var keyCode;
+
+var isActive;
+var hasNumbers;
+var canEdit;
+var shouldUpdate;
+var disabled;
+
+var items = [];
+var messages = [];
+var categories = [];
